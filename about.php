9a2e2f3b92a0c99666fb22e820aae35636b5e352
Eugene_Chan <ilvinchan95@hotmail.com>
2018-07-21 10:54:06 +0900

    give newest status

--- about.php
+++ about.php
@@ -3,6 +3,6 @@
   <img src="">
   <p>Hi,My name is Eugene.I am living in Tokyo now and looking for any jobs in here.Hopefully it works at what i am thinking.</p>
   <p>こんにちは、私はEugeneと申します、今就職活動などの為に東京に住んでいます。</p>
-
+  <p>7/1 日本語N1試験受けました、今は結果発表待ちです。</p>
 </div>
 <?php include("foot.php")?>
